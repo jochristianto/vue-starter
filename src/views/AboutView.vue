23aa29eb07faf51a43cb6099889e9e84494ca626
Jonathan Christianto <jo.christianto@gmail.com>
2024-10-25 23:47:04 +0700

    Setup Shadcn UI integration

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <UiButton> Shadcn UI </UiButton>
   </div>
 </template>
+
+<script lang="js">
+import { Button as UiButton } from '@/components/ui/button'
+
+export default {
+  components: {
+    UiButton,
+  },
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
