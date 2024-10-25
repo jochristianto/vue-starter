a817b21ab87fdb5debc2a648787f48e171a0a161
Jonathan Christianto <jo.christianto@gmail.com>
2024-10-25 23:46:50 +0700

    Setup Tailwind CSS

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -6,13 +8,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
